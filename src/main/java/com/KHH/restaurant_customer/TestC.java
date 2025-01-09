7292f3f304897f9eab69a890431b6f7dedf518c8
NebuchadnezzarV <minseok7694@gmail.com>
2025-01-09 18:06:11 +0900

    [restore] make default setting

---
+++ src/main/java/com/KHH/restaurant_customer/TestC.java
@@ -0,0 +1,16 @@
+package com.KHH.restaurant_customer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/test")
+public class TestC extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
