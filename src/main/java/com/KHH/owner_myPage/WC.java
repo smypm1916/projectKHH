db7395735c1ca0ee85a862684726ac721ea81910
wonho <dutchface498@gmail.com>
2025-01-16 16:12:12 +0900

    [refactor]<third commit>

---
+++ src/main/java/com/KHH/owner_myPage/WC.java
@@ -0,0 +1,21 @@
+package com.KHH.owner_myPage;
+
+import java.io.IOException;
+import java.rmi.ServerException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/WC")
+public class WC extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
